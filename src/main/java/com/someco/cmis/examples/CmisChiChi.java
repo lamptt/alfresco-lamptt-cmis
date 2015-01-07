ae961c994b29d0b0e33b35bf9eb5d3a344470728
chiptk <chiptk@ecoit.asia>
2015-01-07 11:44:01 +0700

    Tạo thư mục test trên 9092
    Tạo thư mục test trên 9092

--- src/main/java/com/someco/cmis/examples/CmisChiChi.java
+++ src/main/java/com/someco/cmis/examples/CmisChiChi.java
@@ -9,28 +9,124 @@ public class CmisChiChi extends CMISExampleBase {
 		
 		CmisChiChi chi = new CmisChiChi();
 		//set connect
-		chi.setServiceUrl("http://192.168.1.51:8080/alfresco/cmisatom");
+		chi.setServiceUrl("http://192.168.1.51:9092/alfresco/cmisatom");
 		
 		//set username and password
 		chi.setUser("admin");
 		chi.setPassword("123456");
 		
 		//create folder, phong, hopso and hoso
-		chi.setFolderName("1_Tài liệu số hóa");
+		chi.setFolderName("");
+		chi.createTestFolder("1_Chitest", thumuc);
+		
+		chi.setFolderName("1_Chitest");
 		chi.createTestFolder("Năm 2001", thumuc);
 		chi.createTestFolder("Năm 2002", thumuc);
 		chi.createTestFolder("Năm 2003", thumuc);
+		chi.createTestFolder("Năm 2004", thumuc);
+		chi.createTestFolder("Năm 2005", thumuc);
 		
-		chi.setFolderName("1_Tài liệu số hóa/Năm 2001");
-		chi.createTestFolder("Phông 11", phong);
-		chi.createTestFolder("Phông 12", phong);
+		chi.setFolderName("1_Chitest/Năm 2001");
+		chi.createTestFolder("Phông 1.1", phong);
+		chi.createTestFolder("Phông 1.2", phong);
 		
-		chi.setFolderName("1_Tài liệu số hóa/Năm 2001/Phông 11");
-		chi.createTestFolder("Hộp số 111", hopso);
+		chi.setFolderName("1_Chitest/Năm 2002");
+		chi.createTestFolder("Phông 2.1", phong);
+		chi.createTestFolder("Phông 2.2", phong);
 		
-		chi.setFolderName("1_Tài liệu số hóa/Năm 2001/Phông 11/Hộp số 111");
-		chi.createTestFolder("Hồ sơ 1111", hoso);
+		chi.setFolderName("1_Chitest/Năm 2003");
+		chi.createTestFolder("Phông 3.1", phong);
+		chi.createTestFolder("Phông 3.2", phong);
 		
+		chi.setFolderName("1_Chitest/Năm 2004");
+		chi.createTestFolder("Phông 4.1", phong);
+		chi.createTestFolder("Phông 4.2", phong);
+		
+		chi.setFolderName("1_Chitest/Năm 2005");
+		chi.createTestFolder("Phông 5.1", phong);
+		chi.createTestFolder("Phông 5.2", phong);
+		
+		chi.setFolderName("1_Chitest/Năm 2001/Phông 1.1");
+		chi.createTestFolder("Hộp số 1.1.1", hopso);
+		chi.createTestFolder("Hộp số 1.1.2", hopso);
+		
+		chi.setFolderName("1_Chitest/Năm 2001/Phông 1.2");
+		chi.createTestFolder("Hộp số 1.2.1", hopso);
+		chi.createTestFolder("Hộp số 1.2.2", hopso);
+		
+		chi.setFolderName("1_Chitest/Năm 2002/Phông 2.1");
+		chi.createTestFolder("Hộp số 2.1.1", hopso);
+		chi.createTestFolder("Hộp số 2.1.2", hopso);
+		
+		chi.setFolderName("1_Chitest/Năm 2002/Phông 2.2");
+		chi.createTestFolder("Hộp số 2.2.1", hopso);
+		chi.createTestFolder("Hộp số 2.2.2", hopso);
+		
+		chi.setFolderName("1_Chitest/Năm 2003/Phông 3.1");
+		chi.createTestFolder("Hộp số 3.1.1", hopso);
+		chi.createTestFolder("Hộp số 3.1.2", hopso);
+		
+		chi.setFolderName("1_Chitest/Năm 2003/Phông 3.2");
+		chi.createTestFolder("Hộp số 3.2.1", hopso);
+		chi.createTestFolder("Hộp số 3.2.2", hopso);
+		
+
+		chi.setFolderName("1_Chitest/Năm 2004/Phông 4.1");
+		chi.createTestFolder("Hộp số 4.1.1", hopso);
+		chi.createTestFolder("Hộp số 4.1.2", hopso);
+		
+		chi.setFolderName("1_Chitest/Năm 2004/Phông 4.2");
+		chi.createTestFolder("Hộp số 4.2.1", hopso);
+		chi.createTestFolder("Hộp số 4.2.2", hopso);
+		
+
+		chi.setFolderName("1_Chitest/Năm 2005/Phông 5.1");
+		chi.createTestFolder("Hộp số 5.1.1", hopso);
+		chi.createTestFolder("Hộp số 5.1.2", hopso);
+		
+		chi.setFolderName("1_Chitest/Năm 2005/Phông 5.2");
+		chi.createTestFolder("Hộp số 5.2.1", hopso);
+		chi.createTestFolder("Hộp số 5.2.2", hopso);
+		
+		chi.setFolderName("1_Chitest/Năm 2001/Phông 1.1/Hộp số 1.1.1");
+		chi.createTestFolder("Hộp số 1.1.1.1", hoso);
+		chi.createTestFolder("Hộp số 1.2.1.1", hoso);
+		
+		chi.setFolderName("1_Chitest/Năm 2001/Phông 1.2/Hộp số 1.2.1");
+		chi.createTestFolder("Hộp số 1.2.1.1", hoso);
+		chi.createTestFolder("Hộp số 1.2.1.2", hoso);
+		
+		chi.setFolderName("1_Chitest/Năm 2002/Phông 2.1/Hộp số 2.1.1");
+		chi.createTestFolder("Hộp số 2.1.1.1", hoso);
+		chi.createTestFolder("Hộp số 2.2.1.1", hoso);
+		
+		chi.setFolderName("1_Chitest/Năm 2002/Phông 2.2/Hộp số 2.2.1");
+		chi.createTestFolder("Hộp số 2.2.1.1", hoso);
+		chi.createTestFolder("Hộp số 2.2.1.2", hoso);
+		
+		chi.setFolderName("1_Chitest/Năm 2003/Phông 3.1/Hộp số 3.1.1");
+		chi.createTestFolder("Hộp số 3.1.1.1", hoso);
+		chi.createTestFolder("Hộp số 3.2.1.1", hoso);
+		
+		chi.setFolderName("1_Chitest/Năm 2003/Phông 3.2/Hộp số 3.2.1");
+		chi.createTestFolder("Hộp số 3.2.1.1", hoso);
+		chi.createTestFolder("Hộp số 3.2.1.2", hoso);
+		
+		chi.setFolderName("1_Chitest/Năm 2004/Phông 4.1/Hộp số 4.1.1");
+		chi.createTestFolder("Hộp số 4.1.1.1", hoso);
+		chi.createTestFolder("Hộp số 4.2.1.1", hoso);
+		
+		chi.setFolderName("1_Chitest/Năm 2004/Phông 4.2/Hộp số 4.2.1");
+		chi.createTestFolder("Hộp số 4.2.1.2", hoso);
+		chi.createTestFolder("Hộp số 4.2.2.2", hoso);
+		
+		chi.setFolderName("1_Chitest/Năm 2005/Phông 5.1/Hộp số 5.1.1");
+		chi.createTestFolder("Hộp số 5.1.1.1", hoso);
+		chi.createTestFolder("Hộp số 5.2.1.1", hoso);
+		
+		chi.setFolderName("1_Chitest/Năm 2005/Phông 5.2/Hộp số 5.2.1");
+		chi.createTestFolder("Hộp số 5.2.1.2", hoso);
+		chi.createTestFolder("Hộp số 5.2.2.2", hoso);
 		//end
 		System.out.println("Done!");
 	}
